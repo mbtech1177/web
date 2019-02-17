7d495c3c1fb5c46379d100f179f6a4d80bab452c
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-02-17 23:13:18 +0300

    add more methods

--- src/instagram/methods.js
+++ src/instagram/methods.js
@@ -1,12 +1,18 @@
-// def get_hashtag_feed(self, hashtag, max_id=''):
-//     url = 'feed/tag/{hashtag}/?max_id={max_id}&rank_token={rank_token}&ranked_content=true&'
-//     url = url.format(
-//         hashtag=hashtag,
-//         max_id=max_id,
-//         rank_token=self.rank_token
-//     )
-//     return self.send_request(url)
-//
+
+function is_user_id(user_id_or_username){
+  return !isNaN(user_id_or_username)
+}
+
+
+export const get_user_info = (self, user_id_or_username) => {
+  if (is_user_id(user_id_or_username)) {
+    const user_id = user_id_or_username
+    return self.send_request(`users/${user_id}/info/`)
+  } else { 
+    const username = user_id_or_username
+    return self.send_request(`users/${username}/usernameinfo/`)
+  }
+}
 
 export const get_hashtag_feed = (self, hashtag, max_id='') => {
     const rank_token = self.rank_token()
@@ -20,46 +26,20 @@ export const get_location_feed = (self, location_id, max_id='') => {
     return self.send_request(url)
 }
 
-// def like(self, media_id):
-//     data = self.json_data({'media_id': media_id})
-//     url = 'media/{media_id}/like/'.format(media_id=media_id)
-//     return self.send_request(url, data)
-//
-// def unlike(self, media_id):
-//     data = self.json_data({'media_id': media_id})
-//     url = 'media/{media_id}/unlike/'.format(media_id=media_id)
-//     return self.send_request(url, data)
-
 export const like = (self, media_id) => {
-  const data = { media_id }
-
-  return self.send_request(`media/${media_id}/like/`, data)
+  return self.send_request(`media/${media_id}/like/`, {})
 }
 
 export const unlike = (self, media_id) => {
-  const data = { media_id }
+  return self.send_request(`media/${media_id}/unlike/`, {})
+}
 
-  return self.send_request(`media/${media_id}/unlike/`, data)
+export const follow = (self, user_id) => {
+  return self.send_request(`friendships/create/${user_id}/`, {})
+}
+
+export const unfollow = (self, user_id) => {
+  return self.send_request(`friendships/destroy/${user_id}/`, {})
 }
 
 
-
-// vvvvvvvvvvvvvvvvv
-
-// def get_location_feed(self, location_id, max_id=''):
-//     url = 'feed/location/{location_id}/?max_id={max_id}&rank_token={rank_token}&ranked_content=true&'
-//     url = url.format(
-//         location_id=location_id,
-//         max_id=max_id,
-//         rank_token=self.rank_token
-//     )
-//     return self.send_request(url)
-//
-// def get_popular_feed(self):
-//     url = 'feed/popular/?people_teaser_supported=1&rank_token={rank_token}&ranked_content=true&'
-//     return self.send_request(url.format(rank_token=self.rank_token))
-
-// export default {
-//   get_hashtag_feed,
-//
-// }
