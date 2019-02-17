60f204d803093a2c95b20fa9ff5f3056011f2029
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-02-18 00:08:36 +0300

    attempt to call a method

--- src/index.js
+++ src/index.js
@@ -15,7 +15,16 @@ window.onload = () => {
 
     instagram.login(username.value, password.value)
       .then(user => alert('logged in as @' + user.full_name))
-      .then(() => window.instagram = instagram)
-      .catch(err => alert(err.message))
+      .then(
+        () => window.instagram = instagram)
+        .catch(err => alert(err.message)
+      )
+
+    instagram.callMethod('get_user_info', 'instagram')
+      .then(user_info => alert(user_info['pk']))
+
+    // const user_info = instagram.callMethod('get_user_info', 'instagram')
+    // alert(user_info)
+    // instagram.callMethod('follow', user_info['pk']).catch(err => alert(err.message))
   }
 }
