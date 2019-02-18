3172f1a2155cd5189747790b629aba419e4cb4b1
Aleksey Bykhun <theisease@gmail.com>
2019-02-19 00:21:23 +0300

    index.html: connect, unlike button

--- chrome-ext/inject.js
+++ chrome-ext/inject.js
@@ -16,16 +16,35 @@ chrome.runtime.onMessage.addListener((message, sender) => {
   } else {
     if (status == 'ok') {
       console.log('inject.js: request success', message)
+      alert(`Request succeeded: ${message}`)
     } else {
       console.log('inject.js: request error', message)
     }
   }
 })
 
-chrome.runtime.sendMessage(null, {
-  method: 'get_user_info',
-  params: []
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.forms[0]
+
+  console.log('form', form)
+  form.onsubmit = function (event) {
+    event.preventDefault()
+    return false
+  }
 })
+
+
+document.getElementById("connect").addEventListener("click", function () {
+
+  chrome.runtime.sendMessage(null, {
+    method: 'get_user_info',
+    params: []
+  })
+
+  // window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*")
+}, false)
+
 
 document.getElementById("likeButton").addEventListener("click", function () {
   const form = document.forms[0]
@@ -34,6 +53,20 @@ document.getElementById("likeButton").addEventListener("click", function () {
 
   chrome.runtime.sendMessage(null, {
     method: 'like',
+    params: [ media_id ]
+  })
+
+  // window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*")
+}, false)
+
+
+document.getElementById("unlikeButton").addEventListener("click", function () {
+  const form = document.forms[0]
+
+  const media_id = form.media_id.value
+
+  chrome.runtime.sendMessage(null, {
+    method: 'unlike',
     params: [ media_id ]
   })
 
