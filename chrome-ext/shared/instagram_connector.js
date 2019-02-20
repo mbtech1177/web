353cd84a6869b7c6192f66bb0a010e544556f3a2
Aleksey Bykhun <theisease@gmail.com>
2019-02-21 01:35:12 +0300

    instagram connector fixes

--- chrome-ext/shared/instagram_connector.js
+++ chrome-ext/shared/instagram_connector.js
@@ -16,7 +16,7 @@ class InstagramConnector {
   kill = () => this.isStopped = true
 
   request = (data) => new Promise((resolve, reject) => {
-    if (this.isStopped) return reject(new InstagramError(null, `Request was killed`))
+    if (this.isStopped) return reject(new Error(`Request was killed`))
 
     // { method, params } = method
 
@@ -24,25 +24,30 @@ class InstagramConnector {
 
     const handler = (message, sender) => {
       const { status, error } = message
-      if (req_id === message.req_id) {
-        chrome.runtime.onMessage && chrome.runtime.onMessage.removeListener(handler)
 
-        console.log('request', data.method, '->', status, message)
+      if (req_id && req_id !== message.req_id) return
 
-        if (status !== 'ok') {
-          reject(new InstagramError(status, error))
-        } else {
-          resolve(message)
-        }
+      chrome.runtime.onMessage && chrome.runtime.onMessage.removeListener(handler)
+
+      console.log('request', data.method, '->', status, message)
+
+      if (status !== 'ok') {
+        reject(new InstagramError(status, error))
+      } else {
+        resolve(message)
       }
     }
+
+    setTimeout(() => reject(new Error(`Request timeout`)), 10000)
 
     chrome.runtime.onMessage && chrome.runtime.onMessage.addListener(handler)
 
     if (chrome.runtime.onMessage) {
+      console.log(`send_message`, null, { req_id, ...data })
       chrome.runtime.sendMessage(null, { req_id, ...data })
     } else {
-      chrome.runtime.sendMessage(this._instaweb_id, { req_id, ...data }, null, handler)
+      console.log(`send_message`, this._instaweb_id, { req_id, ...data }, null, handler)
+      chrome.runtime.sendMessage(this._instaweb_id, data, null, handler)
     }
   })
 
