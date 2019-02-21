4f72da8475c4e8684882e9b7ea765eb4cca6c5b7
Aleksey Bykhun <theisease@gmail.com>
2019-02-22 01:46:13 +0300

    disable scroll-to-top jquery button

--- design/js/sb-admin-2.js
+++ design/js/sb-admin-2.js
@@ -26,16 +26,16 @@
       e.preventDefault();
     }
   });
-
-  // Scroll to top button appear
-  $(document).on('scroll', function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+  //
+  // // Scroll to top button appear
+  // $(document).on('scroll', function() {
+  //   var scrollDistance = $(this).scrollTop();
+  //   if (scrollDistance > 100) {
+  //     $('.scroll-to-top').fadeIn();
+  //   } else {
+  //     $('.scroll-to-top').fadeOut();
+  //   }
+  // });
 
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(e) {
