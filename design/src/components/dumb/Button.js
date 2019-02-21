75e6ee474f169efaad8c0f1e4b0a9f89e327f154
Aleksey Bykhun <theisease@gmail.com>
2019-02-22 00:55:40 +0300

    Button dont redirect to home page

--- design/src/components/dumb/Button.js
+++ design/src/components/dumb/Button.js
@@ -1,5 +1,11 @@
+const onClick = (handler) => (event) => {
+  event.preventDefault()
+
+  return handler()
+}
+
 const Button = props => (
-  <a href="#" onClick={props.onClick} className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+  <a href="#" onClick={onClick(props.onClick)} className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
     {props.children}
   </a>
 )
