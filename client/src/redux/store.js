0f885fb331a55906bf98e7a504543974a3a4ee1b
Aleksey Bykhun <theisease@gmail.com>
2019-02-22 20:12:41 +0300

    save log to localstorage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,19 +6,26 @@ const initialState = {
     status: CONNECTION.UNKNOWN,
     description: '',
   },
-  log: [],
+  log: JSON.parse(localStorage.getItem('log')) || [],
   user: {},
   error: '',
 }
 
 const reducer = (state = initialState, action) => {
   console.log('action', action)
+
   if (action.type === 'PRINT_LOG') {
+    const { line, newLine } = action.payload
+
+    const log = newLine
+      ? [ ...state.log, `<br>`, line ]
+      : [ ...state.log, line ]
+
+    localStorage.setItem('log', JSON.stringify(log))
+
     return {
       ...state,
-      log: action.payload.newLine
-        ? [ ...state.log, `<br>`, action.payload.line ]
-        : [ ...state.log, action.payload.line ],
+      log,
     }
   }
 
