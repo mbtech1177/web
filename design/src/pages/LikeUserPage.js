3cdae38bd825b430122ab631aedf99f9e239cc90
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-02-22 10:49:27 +0300

    rename user page

--- design/src/pages/LikeUserPage.js
+++ design/src/pages/LikeUserPage.js
@@ -1,6 +1,6 @@
-const LikeHashtagPage = () => (
+const LikeUserPage = () => (
   <div className="container-fluid">
-    <h1 className="h3 mb-4 text-gray-800">Like medias by hashtag</h1>
+    <h1 className="h3 mb-4 text-gray-800">Like user medias</h1>
 
     <div className="row">
       <div className="col-lg-6">
