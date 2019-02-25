b053cff070294ffaa0f5fe7c1f94bc4744b43cea
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-02-26 01:56:35 +0300

    add dropdown

--- client/src/components/markup/Topbar.js
+++ client/src/components/markup/Topbar.js
@@ -82,12 +82,32 @@ const __Topbar = (props) => (
 
       {/* <!-- Nav Item - User Information  --> */}
       <li className="nav-item dropdown no-arrow">
-        <a className="nav-link dropdown-toggle" href={props.user ? `https://www.instagram.com/${props.user.username}/` : `#`} target="_blank">
-          <span className="mr-2 d-none d-lg-inline text-gray-600 small">
-            {props.user.username}
-          </span>
-          <img className="img-profile rounded-circle" src={props.user.profile_pic_url} />
-        </a>
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+              {props.user.username}
+            </span>
+            <img className="img-profile rounded-circle" src={props.user.profile_pic_url} />
+          </a>
+          {/* <!-- Dropdown - User Information --> */}
+          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href={props.user ? `https://www.instagram.com/${props.user.username}/` : `#`} target="_blank">
+              <i class="fab fa-instagram fa-md fa-fw mr-2 text-gray-400"></i>
+              You Instagram
+            </a>
+            {/* <a class="dropdown-item" href="#">
+              <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+              Settings
+            </a>
+            <a class="dropdown-item" href="#">
+              <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+              Activity Log
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </a> */}
+          </div>
       </li>
 
     </ul>
