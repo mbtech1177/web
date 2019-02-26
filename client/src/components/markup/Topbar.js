1f7e4f90d40348bafaf95d9f7a96260c27b2658f
Aleksey Bykhun <theisease@gmail.com>
2019-02-26 18:31:56 +0300

    Revert "class -> className"
    This reverts commit 517ae3f31d523a6f5dd8b09413601b6383dd6526.

--- client/src/components/markup/Topbar.js
+++ client/src/components/markup/Topbar.js
@@ -108,29 +108,29 @@ const __Topbar = (props) => (
 
       {props.connection.status === CONNECTION.LOGGED_IN && (
         <li className="nav-item dropdown no-arrow">
-            <a className"nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span className"mr-2 d-none d-lg-inline text-gray-600 small">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="mr-2 d-none d-lg-inline text-gray-600 small">
                 {props.user.username}
               </span>
               <img className="img-profile rounded-circle" src={props.user.profile_pic_url} />
             </a>
             {/* <!-- Dropdown - User Information --> */}
-            <div className"dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-              <a className"dropdown-item" href={props.user ? `https://www.instagram.com/${props.user.username}/` : `#`} target="_blank">
-                <i className"fab fa-instagram fa-md fa-fw mr-2 text-gray-400"></i>
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+              <a class="dropdown-item" href={props.user ? `https://www.instagram.com/${props.user.username}/` : `#`} target="_blank">
+                <i class="fab fa-instagram fa-md fa-fw mr-2 text-gray-400"></i>
                 You Instagram
               </a>
-              {/* <a className"dropdown-item" href="#">
-                <i className"fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+              {/* <a class="dropdown-item" href="#">
+                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                 Settings
               </a>
-              <a className"dropdown-item" href="#">
-                <i className"fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+              <a class="dropdown-item" href="#">
+                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                 Activity Log
               </a>
-              <div className"dropdown-divider"></div>
-              <a className"dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                <i className"fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Logout
               </a> */}
             </div>
