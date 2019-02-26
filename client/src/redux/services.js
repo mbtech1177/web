29016a11cfac58009be6684e546206687cb10199
Aleksey Bykhun <theisease@gmail.com>
2019-02-27 01:06:40 +0300

    add csv,download helpers

--- client/src/redux/services.js
+++ client/src/redux/services.js
@@ -60,6 +60,21 @@ const fetchStats = async () => {
   return data
 }
 
+const getCSV = arr => [ Object.keys(arr[0]), ...arr.map(item => Object.values(item).join(',')) ].join('\n')
+
+const download = (filename, content) => {
+  const element = document.createElement('a')
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content))
+  element.setAttribute('download', filename)
+
+  element.style.display = 'none'
+  document.body.appendChild(element)
+
+  element.click()
+
+  document.body.removeChild(element)
+}
+
 const likePhotosByHashtag = async (hashtag, n, printLog = console.log) => {
 
   if (!instagram.isStopped) {
