c882296226fda880c2d448e91206256da75ee533
Aleksey Bykhun <theisease@gmail.com>
2019-03-03 21:01:37 +0300

    release 0.2.13 open popup only when not logged in

--- chrome-ext/background/services/on_install.js
+++ chrome-ext/background/services/on_install.js
@@ -1,5 +1,7 @@
-chrome.runtime.onInstalled.addListener(function (object) {
-    chrome.tabs.create({ url: "popup/popup.html" }, function (tab) {
-      
-    })
+chrome.runtime.onInstalled.addListener(async function (object) {
+  const { username, password } = await getCredentials()
+
+  if (!username || !password) {
+    chrome.tabs.create({ url: "popup/popup.html" }, function (tab) {})
+  }
 })
